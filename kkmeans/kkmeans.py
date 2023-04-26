8f1943f0fafe27033a5a31dc278525316d32cde7
Johannes Oster <johannes.oster@outlook.at>
2023-04-27 00:28:02 +0200

    Add kkmeans implementation

--- kkmeans/kkmeans.py
+++ kkmeans/kkmeans.py
@@ -1,2 +1,60 @@
-def kkmeans():
-    return "Hello Kernel k-means!"
+
+import numpy as np
+from .kernel_functions import rbf_kernel
+
+def kkmeans(X, n_clusters, kernel_function=rbf_kernel, max_iterations=100, tol=1e-3):
+    """
+    :param X: data matrix
+    :param n_clusters: number of clusters
+    :param kernel_function: kernel function
+    :param max_iter: maximum number of iterations
+    :param tol: tolerance for stopping criterion
+    """
+    n_samples = X.shape[0]
+    K = kernel_function(X)
+    cluster_assignments = np.random.randint(n_clusters, size=n_samples)
+
+    # Initialize variables to track the objective function value and the change in cluster assignments
+    obj_value = np.inf
+    delta_assignments = np.inf
+
+    for _ in range(max_iterations):
+        dist_matrix = np.zeros((n_samples, n_clusters))
+        for cluster in range(n_clusters):
+            cluster_indices = np.where(cluster_assignments == cluster)[0] # find the indices of data points assigned to the current cluster
+            if len(cluster_indices) == 0:
+                """
+                Empty clusters lead to a division by zero in the calculation of the distance matrix.
+                Therefore we just assign a large distance value when an empty cluster is encountered.
+                This way, the algorithm will naturally avoid assigning points to empty clusters.
+                """
+                dist_matrix[:, cluster] = np.inf
+                continue
+            
+            K_XX = K[np.ix_(cluster_indices, cluster_indices)] # get the kernel values for all pairs of data points belonging to the same cluster
+            K_X = K[:, cluster_indices] # extracts the columns of K corresponding to the kernel values between all data points and the data points in the current cluster.
+            
+
+            # distance(i, c) = K(i, i) - (2 * sum(K(i, j)) / |C|) + (sum(K(j, l)) / |C|^2)            
+            dist_matrix[:, cluster] = np.diag(K) - 2 * np.sum(K_X, axis=1) / len(cluster_indices) + np.sum(K_XX) / (len(cluster_indices) ** 2)
+
+        # Reassign data points to the closest cluster centroid
+        new_cluster_assignments = np.argmin(dist_matrix, axis=1)
+
+        # Calculate the change in cluster assignments
+        delta_assignments = np.sum(cluster_assignments != new_cluster_assignments)
+
+        cluster_assignments = new_cluster_assignments
+
+        # Calculate the new objective function value
+        new_obj_value = np.sum([K[i, j] for i, j in zip(range(len(X)), cluster_assignments)])
+
+        converges = np.abs(new_obj_value - obj_value) < tol or delta_assignments == 0
+        if converges:
+            break
+
+        obj_value = new_obj_value
+
+    return cluster_assignments
+
+
--- tests/test_kkmeans.py
+++ tests/test_kkmeans.py
@@ -1,5 +1,57 @@
+import numpy as np
 from kkmeans import kkmeans
 
-def test_kkmeans():
-    assert kkmeans() == "Hello Kernel k-means!"
+def test_kkmeans_raises_on_empty_dataset_error():
+    X = np.array([])
+    try:
+        kkmeans(X, 3)
+        assert False, "Should have raised ValueError"
+    except ValueError:
+        pass
 
+def test_kkmeans_one_dimensional_dataset():
+    X = np.array([[1], [2], [3], [4], [5]])
+    n_clusters = 3
+    assignments = kkmeans(X, n_clusters)
+    assert len(assignments) == len(X)
+    assert all(0 <= a < n_clusters for a in assignments)
+
+def test_kkmeans_multi_dimensional_dataset():
+    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+    n_clusters = 2
+    assignments = kkmeans(X, n_clusters)
+    assert len(assignments) == len(X)
+    assert all(0 <= a < n_clusters for a in assignments)
+
+def test_kernel_kmeans_consistency():
+    """
+    Check that the majority of runs produce consistent cluster assignments.
+    1. Create a simple synthetic dataset with two distinct clusters.
+    2. Calculate the pairwise Hamming distance (number of values that are different)
+       between the cluster assignments. If the Hamming distance is either small or huge, then the
+       cluster assignments are consistent.
+    
+    """
+    threshhold = 0.8 # 80% or more of runs should be consistent
+
+    cluster_1 = np.random.randn(50, 2) + np.array([0, 0])
+    cluster_2 = np.random.randn(50, 2) + np.array([5, 5])
+    X = np.vstack((cluster_1, cluster_2))
+
+    num_runs = 5
+    n_clusters = 2
+    assignments = []
+
+    for _ in range(num_runs):
+        cluster_assignments = kkmeans(X, n_clusters)
+        assignments.append(cluster_assignments)
+
+    num_consistent_runs = 0
+    for i in range(num_runs):
+        for j in range(i+1, num_runs):
+            hamming_distance = np.sum(assignments[i] != assignments[j])
+            if hamming_distance <= 5 or hamming_distance >= 95:
+                num_consistent_runs += 1
+
+    number_of_runs = num_runs * (num_runs - 1) / 2
+    assert num_consistent_runs / number_of_runs >= threshhold
