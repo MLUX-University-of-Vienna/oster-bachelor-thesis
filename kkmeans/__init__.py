6221e79c18a03f31700a1b448765b5b8ac2de3c1
Johannes Oster <johannes.oster@outlook.at>
2023-07-03 16:06:08 +0200

    Implement kmeans++ cluster initiatlization

--- kkmeans/__init__.py
+++ kkmeans/__init__.py
@@ -1,2 +1,3 @@
+from .kkmeans import kmeans_plusplus
 from .kkmeans import kkmeans
 from .kernel_functions import rbf_kernel
--- kkmeans/kkmeans.py
+++ kkmeans/kkmeans.py
@@ -2,17 +2,79 @@
 import numpy as np
 from .kernel_functions import rbf_kernel
 
-def kkmeans(X, n_clusters, kernel_function=rbf_kernel, max_iterations=100, tol=1e-3):
+
+def rand_cluster_assignment(X, n_clusters):
     """
-    :param X: data matrix
-    :param n_clusters: number of clusters
-    :param kernel_function: kernel function
-    :param max_iter: maximum number of iterations
-    :param tol: tolerance for stopping criterion
+    random initialization algorithm.
+
+    Parameters:
+        X (np.ndarray): (n_samples, n_features) array
+        n_clusters (int): the number of clusters
+
+    Returns:
+        np.ndarray: cluster assignments where the i-th entry represents the cluster assignment of the i-th data point.
     """
+    return np.random.randint(n_clusters, size=X.shape[0])
+
+
+def kmeans_plusplus(X, n_clusters):
+    """
+    k-means++ initialization algorithm.
+
+    Parameters:
+        X (np.ndarray): (n_samples, n_features) array
+        n_clusters (int): the number of clusters
+
+    Returns:
+        np.ndarray: cluster assignments where the i-th entry represents the cluster assignment of the i-th data point.
+    """
+
+    # First randomly choose one data point as first centroid
+    centroids = [X[np.random.randint(X.shape[0])]]
+
+    for _ in range(1, n_clusters):
+        # Compute the distance from each data point to the nearest centroid
+        dist = np.array([min([np.inner(c-x, c-x)
+                        for c in centroids]) for x in X])
+        # Compute the probabilities
+        probs = dist/dist.sum()
+        # Add one new data point as a centroid
+        cumulative_probs = np.cumsum(probs)
+        r = np.random.rand()
+        for j, p in enumerate(cumulative_probs):
+            if r < p:
+                i = j
+                break
+        centroids.append(X[i])
+
+    return np.array([np.argmin([np.inner(c-x, c-x) for c in centroids]) for x in X])
+
+
+def kkmeans(
+        X,
+        n_clusters,
+        kernel_function=rbf_kernel,
+        initial_cluster_assignments=rand_cluster_assignment,
+        max_iterations=100,
+        tol=1e-3):
+    """
+    Performs kernel k-means clustering on a given data set X.
+
+    Parameters:
+        X (ndarray): input data matrix of shape (n_samples, n_features).
+        n_clusters (int): number of clusters to form.
+        kernel_function (callable, optional): kernel function to use (default=rbf_kernel).
+        rand_cluster_assignment (callable, optional): function to generate initial cluster assignments (default=rand_cluster_assignment).
+        max_iterations (int, optional): maximum number of iterations (default=100)
+        tol (float, optional): tolerance to determine convergence (default=1e-3)
+
+    Returns:
+        ndarray: cluster assignments for each data point in X.
+    """
+
     n_samples = X.shape[0]
     K = kernel_function(X)
-    cluster_assignments = np.random.randint(n_clusters, size=n_samples)
+    cluster_assignments = initial_cluster_assignments(X, n_clusters)
 
     # Initialize variables to track the objective function value and the change in cluster assignments
     obj_value = np.inf
@@ -21,7 +83,8 @@ def kkmeans(X, n_clusters, kernel_function=rbf_kernel, max_iterations=100, tol=1
     for _ in range(max_iterations):
         dist_matrix = np.zeros((n_samples, n_clusters))
         for cluster in range(n_clusters):
-            cluster_indices = np.where(cluster_assignments == cluster)[0] # find the indices of data points assigned to the current cluster
+            # find the indices of data points assigned to the current cluster
+            cluster_indices = np.where(cluster_assignments == cluster)[0]
             if len(cluster_indices) == 0:
                 """
                 Empty clusters lead to a division by zero in the calculation of the distance matrix.
@@ -30,31 +93,34 @@ def kkmeans(X, n_clusters, kernel_function=rbf_kernel, max_iterations=100, tol=1
                 """
                 dist_matrix[:, cluster] = np.inf
                 continue
-            
-            K_XX = K[np.ix_(cluster_indices, cluster_indices)] # get the kernel values for all pairs of data points belonging to the same cluster
-            K_X = K[:, cluster_indices] # extracts the columns of K corresponding to the kernel values between all data points and the data points in the current cluster.
-            
 
-            # distance(i, c) = K(i, i) - (2 * sum(K(i, j)) / |C|) + (sum(K(j, l)) / |C|^2)            
-            dist_matrix[:, cluster] = np.diag(K) - 2 * np.sum(K_X, axis=1) / len(cluster_indices) + np.sum(K_XX) / (len(cluster_indices) ** 2)
+            # get the kernel values for all pairs of data points belonging to the same cluster
+            K_XX = K[np.ix_(cluster_indices, cluster_indices)]
+            # extracts the columns of K corresponding to the kernel values between all data points and the data points in the current cluster.
+            K_X = K[:, cluster_indices]
+
+            # distance(i, c) = K(i, i) - (2 * sum(K(i, j)) / |C|) + (sum(K(j, l)) / |C|^2)
+            dist_matrix[:, cluster] = np.diag(K) - 2 * np.sum(K_X, axis=1) / len(
+                cluster_indices) + np.sum(K_XX) / (len(cluster_indices) ** 2)
 
         # Reassign data points to the closest cluster centroid
         new_cluster_assignments = np.argmin(dist_matrix, axis=1)
 
         # Calculate the change in cluster assignments
-        delta_assignments = np.sum(cluster_assignments != new_cluster_assignments)
+        delta_assignments = np.sum(
+            cluster_assignments != new_cluster_assignments)
 
         cluster_assignments = new_cluster_assignments
 
         # Calculate the new objective function value
-        new_obj_value = np.sum([K[i, j] for i, j in zip(range(len(X)), cluster_assignments)])
+        new_obj_value = np.sum(
+            [K[i, j] for i, j in zip(range(len(X)), cluster_assignments)])
 
-        converges = np.abs(new_obj_value - obj_value) < tol or delta_assignments == 0
+        converges = np.abs(
+            new_obj_value - obj_value) < tol or delta_assignments == 0
         if converges:
             break
 
         obj_value = new_obj_value
 
     return cluster_assignments
-
-
