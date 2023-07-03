c8c8992f2f16161c38b32018227b5979937744ac
Johannes Oster <johannes.oster@outlook.at>
2023-07-03 16:05:32 +0200

    Add median distance to heuristically set sigma for rbf

--- kkmeans/kernel_functions.py
+++ kkmeans/kernel_functions.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 
-def rbf_kernel(X, sigma=1.0):
+def median_distance(X):
+    pairwise_dist = np.sqrt(np.sum((X[:, np.newaxis] - X) ** 2, axis=-1))
+    sigma = np.median(pairwise_dist)
+    return sigma
+
+
+def rbf_kernel(X, sigma=None):
     """
     Radial basis function kernel to compute the pairwise similarty within a single dataset. 
     K(X, Y) = exp(-||x_i - x_j||^2/2σ^2)
@@ -13,6 +19,9 @@ def rbf_kernel(X, sigma=1.0):
         np.ndarray: (n_samples, n_samples) Kernel Matrix, where each entry (i, j) represents
                     the distance between data points i and j.
     """
+
+    if sigma is None:
+        sigma = median_distance(X)
 
     if len(X.shape) == 1:
         raise ValueError("X must be at least a 2-dimensional array.")
