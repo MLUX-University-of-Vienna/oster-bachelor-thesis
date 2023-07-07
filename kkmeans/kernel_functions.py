adb2993e46c7ccc8f6aa43fad798916f02f58096
Johannes Oster <johannes.oster@outlook.at>
2023-07-07 20:50:33 +0200

    Update rbf kernel

--- kkmeans/kernel_functions.py
+++ kkmeans/kernel_functions.py
@@ -1,12 +1,6 @@
 import numpy as np
 
 
-def median_distance(X):
-    pairwise_dist = np.sqrt(np.sum((X[:, np.newaxis] - X) ** 2, axis=-1))
-    sigma = np.median(pairwise_dist)
-    return sigma
-
-
 def rbf_kernel(X, sigma=None):
     """
     Radial basis function kernel to compute the pairwise similarty within a single dataset. 
@@ -20,11 +14,14 @@ def rbf_kernel(X, sigma=None):
                     the distance between data points i and j.
     """
 
-    if sigma is None:
-        sigma = median_distance(X)
-
-    if len(X.shape) == 1:
+    if X.ndim < 2:
         raise ValueError("X must be at least a 2-dimensional array.")
 
     sqdist = np.sum((X[:, np.newaxis] - X) ** 2, axis=-1)
+
+    if sigma is None:
+        sigma = np.sqrt(np.median((sqdist))) or 1.0
+    elif sigma <= 0:
+        raise ValueError("sigma must be a positive number.")
+
     return np.exp(-sqdist / (2 * sigma**2))
